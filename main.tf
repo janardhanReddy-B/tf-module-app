09c6124d3ceb606d9892310581cf60bf966bfafc
JanardhanReddy <bommireddy246@gmail.com>
2023-07-31 22:39:13 +0100

    prometheus

--- main.tf
+++ main.tf
@@ -78,9 +78,10 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile = aws_iam_instance_profile.profile.name
 
-  tags = {
+  tags = merge ({
     Name = "${var.component}-${var.env}"
-  }
+  },
+  var.tags)
 }
 #dns
 resource "aws_route53_record" "web" {
