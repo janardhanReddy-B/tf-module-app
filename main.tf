697cbe6a0202df12c379d57678ccdc361e2d1c31
JanardhanReddy <bommireddy246@gmail.com>
2023-08-08 01:21:13 +0100

    terraform

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_lb_listener_rule" "static" {
 
   condition {
     host_header {
-      values = ["${var.component}-${var.env}.devopsbjr.online"]
+      values = ["${local.dns_name}.devopsbjr.online"]
     }
   }
 }
